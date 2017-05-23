7141b330c06ac0e6fd3c8b40d45ce04650a28791
neil.jia <neil.jia@elitesland.com>
2017-05-23 11:05:37 +0800

    增加:通过@Update注解完成Mybatis的数据更新操作

--- src/main/java/com/mytest/controller/CourseController.java
+++ src/main/java/com/mytest/controller/CourseController.java
@@ -1,12 +1,14 @@
 package com.mytest.controller;
 
 import com.mytest.pojo.Course;
-import com.mytest.pojo.Student;
 import com.mytest.service.CourseService;
+import com.mytest.util.CommonTool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -25,6 +27,23 @@ public class CourseController {
         List<Course> list = courseService.getCourseList();
         modelMap.put("courses", list );
         return "course/courseList";
+    }
+
+    @RequestMapping(value = "/course/update", method = RequestMethod.GET)
+    @ResponseBody
+    public String update(Integer id){
+        Course course = courseService.getCourse(id);
+        if(course == null){
+            return "课程数据不存在，courseId=" + id;
+        }
+        //修改描述中的三位随机数
+        String remark = course.getRemark();
+        remark = remark.substring(0,remark.length()-3);
+        remark += CommonTool.getFixLenthString(3);
+        course.setRemark(remark);
+        //更新数据
+        courseService.updateCourse(course);
+        return "课程数据已更新，courseId=" + id + ", remark=" + remark;
     }
 
 }
